b2835a7026cc8144472dd730d0ed1908cbf78912
abbadhon <urbano@email.com>
2014-05-24 21:39:00 +0100

    Cachematrix
    Contains functions to demonstrate use of scoping features in R.
    From the Programming R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a setters and getters for object of type matrix
+## that will be inverted  by other function
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  matinv <- NULL
+  set <- function(y) {
+      x <<- y
+      matinv <<- NULL
+  }
+  get <- function() x
+  setinverse<- function(inverse) matinv <<- inverse
+  getinverse <- function() matinv
+# Return list of getter/setter
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve returns the matrix inverse from makeCacheMatrix function.
+## If the cached inverse is available, cacheSolve retrieves it, while if
+## not, it computes, caches, and returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  matinv <- x$getinverse()
+  if (is.null(matinv)) {
+    matinv <- solve(x$get())
+    x$setinverse(matinv)
+  } else {
+      message("getting cached inverse matrix")
+  }  
+  return(matinv)
 }
